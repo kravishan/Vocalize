559e083175cf01de121f25ecea251aeca361c94b
Kavindu Ravishan <ravishankavindu@gmail.com>
2023-11-30 16:46:17 +0200

    Update API endpoints

--- client/places.js
+++ client/places.js
@@ -1,7 +1,7 @@
 // places.js (Client-side code)
 
 // Fetch the Google Maps API script from your server
-fetch('https://coral-app-mbl5v.ondigitalocean.app/server/google-maps-script')
+fetch('https://vocalizer.dev/server/google-maps-script')
   .then(response => response.text())
   .then(script => {
     // Execute the retrieved script
