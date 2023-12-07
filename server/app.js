9dd8f14c40139bba27a8694077810f38506f6466
Kavindu Ravishan <ravishankavindu@gmail.com>
2023-12-07 19:12:06 +0200

    Add firebase-admin package

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 const FormData = require('form-data');
 const multer = require('multer');
+const admin = require('firebase-admin');
 
 require('dotenv').config();
 
