a9bcaa103f073b28f9904034aec896931b64d175
Kavindu Ravishan <ravishankavindu@gmail.com>
2023-12-10 02:20:08 +0200

    Fix proximity check and add review reminder notifications

--- client/places.js
+++ client/places.js
@@ -1,7 +1,6 @@
 // places.js (Client-side code)
 
 // Fetch the Google Maps API script from your server
-// fetch('http://localhost:3000/google-maps-script')
 fetch('https://vocalizer.dev/server/google-maps-script')
   .then(response => response.text())
   .then(script => {
@@ -33,7 +32,7 @@ function fetchAndDisplayNearbyRestaurants() {
         // Define the request to fetch nearby restaurants
         const request = {
           location: userLocation,
-          radius: 200, 
+          radius: 200,
           types: ["restaurant"],
         };
 
@@ -47,6 +46,24 @@ function fetchAndDisplayNearbyRestaurants() {
   } else {
     console.error("Geolocation is not supported by this browser.");
   }
+}
+
+// Function to calculate the distance between two sets of coordinates using the Haversine formula
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  const R = 6371; // Radius of the Earth in kilometers
+  const dLat = degToRad(lat2 - lat1);
+  const dLon = degToRad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c; // Distance in kilometers
+  return distance;
+}
+
+// Function to convert degrees to radians
+function degToRad(degrees) {
+  return degrees * (Math.PI / 180);
 }
 
 
@@ -76,9 +93,58 @@ function handleResults(results, status) {
 
       // Append the list item to the restaurant list
       restaurantList.appendChild(listItem);
+
+      // Check if the user has not reviewed this restaurant yet
+      const reviewedRestaurants = JSON.parse(localStorage.getItem("reviewedRestaurants")) || [];
+      if (!reviewedRestaurants.includes(result.place_id)) {
+        // Check proximity and trigger notifications
+        const userLocation = JSON.parse(localStorage.getItem("userLocation"));
+        if (userLocation) {
+          const isInProximity = checkProximity(userLocation, result);
+          if (isInProximity) {
+            triggerNotification(result);
+          }
+        }
+      }
     }
   } else {
     console.error("Error fetching restaurants:", status);
+  }
+}
+
+// Function to check proximity
+function checkProximity(userLocation, restaurant) {
+  // Use your existing calculateDistance function
+  const distance = calculateDistance(
+    userLocation.lat,
+    userLocation.lng,
+    restaurant.geometry.location.lat(),
+    restaurant.geometry.location.lng()
+  );
+
+  // Set a suitable proximity threshold (e.g., 5 meters)
+  const proximityThreshold = 200;
+
+  return distance < proximityThreshold;
+}
+
+// Function to trigger notifications
+function triggerNotification(restaurant) {
+  // Use the Notification API to display a notification
+  if (Notification.permission === 'granted') {
+    const notification = new Notification('Review Reminder', {
+      body: `You're near ${restaurant.name}. Leave a review and help others.`,
+      icon: 'favicon.ico',
+    });
+
+    notification.onclick = function () {
+      // Handle notification click (e.g., open the app or the restaurant's page)
+    };
+
+    // Add the reviewed restaurant to the local storage to avoid repeated notifications
+    const reviewedRestaurants = JSON.parse(localStorage.getItem("reviewedRestaurants")) || [];
+    reviewedRestaurants.push(restaurant.place_id);
+    localStorage.setItem("reviewedRestaurants", JSON.stringify(reviewedRestaurants));
   }
 }
 
@@ -87,8 +153,3 @@ window.onload = function () {
   console.log("Page loaded. Fetching and displaying nearby restaurants.");
   fetchAndDisplayNearbyRestaurants();
 };
-
-
-
-
- 
