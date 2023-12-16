b9070ccc779135dc3ebdc4a150a7c951cd6f6537
Kavindu Ravishan <ravishankavindu@gmail.com>
2023-12-16 21:59:10 +0200

    Add PWA installation guide notification

--- client/custom.js
+++ client/custom.js
@@ -68,3 +68,59 @@ window.addEventListener('scroll', function() {
 });
 
 
+
+
+
+/////////////////////////////////////
+
+
+// script.js or custom.js
+
+// Function to show PWA installation guide notification
+function showPWAInstallNotification() {
+    const notification = document.getElementById('pwa-install-notification');
+    if (notification) {
+      notification.style.display = 'block';
+    }
+  }
+  
+  // Function to close the notification
+  function closeNotification() {
+    const notification = document.getElementById('pwa-install-notification');
+    if (notification) {
+      notification.style.display = 'none';
+    }
+  }
+  
+  // Function to handle the PWA installation guide
+function showPWAInstallationGuide() {
+    // Detect the operating system
+    const operatingSystem = getOperatingSystem();
+  
+    // Customize this function based on your PWA installation guide
+    console.log(`Follow the installation guide for your ${operatingSystem} device.`);
+  }
+  
+  // Function to get the operating system
+  function getOperatingSystem() {
+    const platform = navigator.platform.toLowerCase();
+  
+    if (platform.includes('win')) {
+      return 'Windows';
+    } else if (platform.includes('mac')) {
+      return 'Mac';
+    } else if (platform.includes('linux')) {
+      return 'Linux';
+    } else if (platform.includes('iphone') || platform.includes('ipad') || platform.includes('ipod')) {
+      return 'iOS';
+    } else if (platform.includes('android')) {
+      return 'Android';
+    } else {
+      return 'Unknown';
+    }
+  }
+  
+  
+  // Show PWA installation guide notification after 2 seconds
+  setTimeout(showPWAInstallNotification, 1000);
+  
