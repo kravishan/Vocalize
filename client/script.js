56324f03a542e24652246707750fc744193e5096
Kavindu Ravishan <ravishankavindu@gmail.com>
2023-12-17 23:32:48 +0200

    Update line-height in results.css and remove console.log in script.js

--- client/script.js
+++ client/script.js
@@ -589,11 +589,11 @@ async function generateImprovedReviewWithStars(globalWhisperText, selectedOveral
         // Retrieve the stored user location from localStorage
         var storedUserLocation = localStorage.getItem("userLocation");
         var userLocation = JSON.parse(storedUserLocation);
-        console.log(userLocation);
 
         // Redirect to the result page with parameters
         const resultPageURL = `./result.html?userLocation=${encodeURIComponent(JSON.stringify(userLocation))}&whisperText=${encodeURIComponent(globalWhisperText)}&overallStarCount=${selectedOverallStarCount}&foodRating=${foodRating}&serviceRating=${serviceRating}&atmosphereRating=${atmosphereRating}&improvedReviewWithStars=${encodeURIComponent(improvedReviewWithStars)}`;
         window.location.href = resultPageURL;
+
 
     } catch (error) {
         console.error('Error:', error);
