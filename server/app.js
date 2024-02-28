332d616c34b42bd1949449a722c32acf266cdde7
kravishan <ravishankavindu@gmail.com>
2024-02-28 17:52:56 +0200

    Refine review process: Added text readability requirement

--- server/app.js
+++ server/app.js
@@ -291,6 +291,7 @@ app.post('/refine-review', async (req, res) => {
       "Please ensure your genarated review english level is similar to original review english level. first please identify english level of review and genarate according to it. most of time english level is a1 or a2. dont use any fanzy english words if user review has simple english. this is a must",
       "If user ask to refine only one part of the review like rewrite only one insident, you need to refine only that insident. You dont need to refine the whole review. keep the other parts like previous and change the part what user ask for. This is a super must must",
       "Use only simple english words, dont use any fanzy words. review content shold be understandable for everyone. This is a very very must",
+      "Text readability is important. Please ensure the refined review is easy to read and understand.",
       "If you cannot genarate genarate a results, Please send a message like 'Sorry, we cannot refine this review' to user."
       // "Please ensure that the refined review maintains the original sentiment and tone unless user request to change it by refineInstructions.",
       // "If the user requests additional information, the temperature should be set to 0.1 max it can be goes upto 0.2. If the user requests to make the review more creative, the temperature should be set to 0.4 max it can be goes upto 0.6",
